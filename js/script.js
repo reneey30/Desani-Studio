2503bc0bbfdcea6f4661e77bcd33d0c3306611e8
Irene Njeru <irenenjeru8@gmail.com>
2022-03-11 22:27:57 +0300

    add portifolio to index.html,modified js and css

--- js/script.js
+++ js/script.js
@@ -1,35 +1,36 @@
 
   
-$(document).ready(function(){
-    $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
-      $("#development").show('1500');
-    });
-    $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#development-image").slideDown('1500');
-    });
-    // $(document).ready(function() {
-    // $("#img1").click(function() {
-    //     $("#img1").toggle();
-    //       $("#design").toggle();
-    //     });
+// $(document).ready(function(){
+//     $("#development-image").click(function(){
+//       $("#development-image").slideDown('1500').hide('1000');
+//       $("#development").show('1500');
+//     });
+//     $("#development").click(function(){
+//       $("#development").slideUp('1500');
+//       $("#development-image").slideDown('1500');
+    // });
+    $(document).ready(function() {
+    $("#img1").click(function() {
+        $("#img1").toggle();
+          $("#design").toggle();
+        });
 
-    //     $("#img2").click(function() {
-    //       $("#img2").toggle();
-    //         $("#develop").toggle();
-    //       });
+        $("#img2").click(function() {
+          $("#img2").toggle();
+            $("#develop").toggle();
+          });
 
-    //       $("#develop").click(function() {
-    //         $("#img2").toggle();
-    //           $("#develop").toggle();
-    //         });
+          $("#develop").click(function() {
+            $("#img2").toggle();
+              $("#develop").toggle();
+            });
 
-    //         $("#img3").click(function() {
-    //           $("#img3").toggle();
-    //             $("#product").toggle();
-    //           });
-    //           $("#product").click(function() {
-    //             $("#img3").toggle();
-    //               $("#product").toggle();
-    //             });
+            $("#img3").click(function() {
+              $("#img3").toggle();
+                $("#product").toggle();
+              });
+              $("#product").click(function() {
+                $("#img3").toggle();
+                  $("#product").toggle();
+                });
+                
