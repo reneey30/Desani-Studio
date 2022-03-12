67357bb1a447f8c45f7c78f04c86cee717d37a5d
Irene Njeru <irenenjeru8@gmail.com>
2022-03-12 15:08:09 +0300

    fix linking of background images and fix show and hide functon in what we do

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ $(document).ready(function(){
   $(document).ready(function(){
     $("#img1").click(function(){
       $("#img1").slideDown('1500').hide('1000');
-      $("#design").show('1500');
+      $(".design").show('1500');
     });
-    $("#design").click(function(){
-      $("#design").slideUp('1500');
+    $(".design").click(function(){
+      $(".design").slideUp('1500');
       $("#img1").slideDown('1500');
     });
   });
