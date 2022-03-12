346b2a376516ab3e40ff49633cf8a58d30528e12
Irene Njeru <irenenjeru8@gmail.com>
2022-03-12 13:15:49 +0300

    add mailchimp link and add function on js script

--- js/script.js
+++ js/script.js
@@ -99,10 +99,20 @@ $(document).ready(function(){
   $("#pc8").mouseenter(function(){
     $(".txt8").show(2000); 
 });
-
-  function submit(form){
-    var name= document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    alert("submmission received" + name);
-  }
   
+  $(document).ready(function(){
+    $("form#form34B").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
